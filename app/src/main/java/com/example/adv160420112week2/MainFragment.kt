334660ce3f8c51a1c47953a96f1602ccce22ad75
StevenLabina <126638426+StevenLabina@users.noreply.github.com>
2023-03-08 10:49:26 +0700

    adv160420112Week2

---
+++ app/src/main/java/com/example/adv160420112week2/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.example.adv160420112week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class MainFragment : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+}
