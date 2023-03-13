d2eda3143f5a1817d4cd14f7fbeacea375567310
StevenLabina <126638426+StevenLabina@users.noreply.github.com>
2023-03-13 21:34:53 +0700

    adv160420112Week2

--- app/src/main/java/com/example/adv160420112week2/GameFragment.kt
+++ app/src/main/java/com/example/adv160420112week2/GameFragment.kt
@@ -1,22 +1,26 @@
 package com.example.adv160420112week2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_game.view.*
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_result.*
+import java.util.*
+import kotlin.random.Random
 
+import kotlin.random.Random.Default.nextInt
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
 class GameFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,5 +30,35 @@ class GameFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val number1 = Random.nextInt(1,100)
+        val number2 = Random.nextInt(1,100)
+        txtNumber1.setText(number1.toString())
+        txtNumber2.setText(number2.toString())
+        var finalScore = 0
+        val result = (number1 + number2).toString()
+        btnSubmit.setOnClickListener {
+            var userAnswer = txtAnswer.text.toString()
+            if (result == userAnswer){
+                finalScore++
+                val action = GameFragmentDirections.actionResultFragment(finalScore)
+                Navigation.findNavController(it).navigate(action)
 
+            }
+            if (result != userAnswer){
+                val action = GameFragmentDirections.actionResultFragment(finalScore)
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+        if(arguments != null) {
+            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
+            txtTurn.text = "$playerName's Turn"
+        }
+    }
 }
+
+
+
+
+
--- app/src/main/java/com/example/adv160420112week2/ResultFragment.kt
+++ app/src/main/java/com/example/adv160420112week2/ResultFragment.kt
@@ -6,35 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_main.*
+import com.example.adv160420112week2.ResultFragmentDirections.Companion.actionMainFragment
+import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_result.*
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-class MainFragment : Fragment() {
-
-
-
-
-
+class ResultFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_result, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnStart.setOnClickListener {
-            val playerName = txtName.text.toString()
-            val action = MainFragmentDirections.actionGameFragment(playerName)
+        btnBackMain.setOnClickListener {
+            val action = ResultFragmentDirections.actionMainFragment()
             Navigation.findNavController(it).navigate(action)
         }
+        if(arguments != null) {
+            val finalScore = ResultFragmentArgs.fromBundle(requireArguments()).finalScore
+            txtScore.text = "Your score is $finalScore"
+        }
     }
-
-
-
 }
